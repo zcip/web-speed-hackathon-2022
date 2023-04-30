590a3fded73b1337d18fe3abc14d66728d546cad
zcip <zcip.web@gmail.com>
2023-04-30 15:25:31 +0900

    不要なshimを削除しbrowserlistで最新のchromeのみ対応

--- webpack.config.js
+++ webpack.config.js
@@ -36,16 +36,7 @@ module.exports = [
           use: {
             loader: "babel-loader",
             options: {
-              presets: [
-                [
-                  "@babel/preset-env",
-                  {
-                    modules: "cjs",
-                    spec: true,
-                  },
-                ],
-                "@babel/preset-react",
-              ],
+              presets: [["@babel/preset-env", {}], "@babel/preset-react"],
             },
           },
         },
