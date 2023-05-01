899d98828963b32e6c000c859323286bd82f1564
zcip <zcip.web@gmail.com>
2023-05-01 20:11:58 +0900

    画像にaltをつける

--- src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
+++ src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
-export const Img = styled.img`
+export const Img = styled.img.attrs({ alt: "" })`
   object-fit: cover;
   object-position: center center;
   max-width: 100%;
   height: auto;
-`
+`;
 
 export const ImageW16H9 = styled(Img)`
   aspect-ratio: 16 / 9;
-`
+`;
 
 export const ImageW1H1 = styled(Img)`
   aspect-ratio: 1 / 1;
-`
+`;
