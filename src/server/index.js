f10f14cc79454d3e3edd7765a128fa7ad7195eeb
zcip <zcip.web@gmail.com>
2023-05-01 17:00:33 +0900

    既存のcache-controlを削除

--- src/server/index.js
+++ src/server/index.js
@@ -37,11 +37,6 @@ server.addHook("onRequest", async (req, res) => {
   }
 });
 
-server.addHook("onRequest", async (req, res) => {
-  res.header("Cache-Control", "no-cache, no-store, no-transform");
-  res.header("Connection", "close");
-});
-
 server.register(apiRoute, { prefix: "/api" });
 server.register(spaRoute);
 
